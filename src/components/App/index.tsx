5a524f24b54dc1175028856fca8739e1018125e4
Yasir Nadeem <m-yasir.dev@protonmail.com>
2021-05-07 22:22:22 +0500

    Add Obfuscation. Add .env support

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -3,6 +3,7 @@ import * as React from "react";
 function App() {
     React.useEffect(() => {
         console.log('Hello world');
+        console.log("ENV: ", process.env);
     }, []);
     return <div>Hello World! Welcome to React App with Webpack!</div>;
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,45 +1,49 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const DotEnvPlugin 			= require("dotenv-webpack");
+const HtmlWebpackPlugin 	= require("html-webpack-plugin");
+const MiniCssExtractPlugin 	= require("mini-css-extract-plugin");
+const path 					= require("path");
+const TerserWebpackPlugin 	= require("terser-webpack-plugin");
+const webpack				= require("webpack");
 module.exports = {
 	entry: "./src/index.tsx",
 	target: "web",
 	mode: "development",
 	output: {
 		path: path.resolve(__dirname, "dist"),
-		filename: "[name].bundle.js",
+		filename: "[name].bundle.js"
 	},
 	resolve: {
-		extensions: [".js", ".jsx", ".json", ".ts", ".tsx"],
+		extensions: [".js", ".jsx", ".json", ".ts", ".tsx"]
 	},
 	module: {
 		rules: [
 			{
 				test: /\.(ts|tsx)$/,
-				loader: "ts-loader",
+				loader: "ts-loader"
 			},
 			{
 				enforce: "pre",
 				test: /\.js$/,
-				loader: "source-map-loader",
+				loader: "source-map-loader"
 			},
 			{
 				test: /\.css$/,
-				loader: "css-loader",
-			},
-		],
+				loader: "css-loader"
+			}
+		]
 	},
 	plugins: [
+		new TerserWebpackPlugin({
+			parallel: true
+		}),
+		new webpack.NoEmitOnErrorsPlugin(),
+		new DotEnvPlugin(),
 		new HtmlWebpackPlugin({
 			title: "Development",
-			template: path.resolve(
-				__dirname,
-				"public",
-				"index.html"
-			),
+			template: path.resolve(__dirname, "public", "index.html")
 		}),
 		new MiniCssExtractPlugin({
-			filename: "./public/styles.css",
-		}),
-	],
+			filename: "./public/styles.css"
+		})
+	]
 };
